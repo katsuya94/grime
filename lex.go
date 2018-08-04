ebf957a60935cabef24c4540889df1dceecc57a4
atateno <adrien.tateno@getbraintree.com>
2018-08-04 00:06:22 -0400

    Partially implement characters

--- lex.go
+++ lex.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"strings"
 	"unicode"
+	"encoding/hex"
 )
 
 type Lexeme interface{}
@@ -14,6 +15,7 @@ type Lexeme interface{}
 type Identifier string
 type Boolean bool
 type Number string
+type Character rune
 type LeftParenthesis struct{}
 type RightParenthesis struct{}
 
@@ -165,6 +167,12 @@ func digit(r rune) bool {
 	return 48 <= r && r < 58
 }
 
+func hexDigit(r rune) bool {
+	return digit(r) ||
+		(65 <= r && r < 71) ||
+		(97 <= r && r < 103)
+}
+
 func specialSubsequent(r rune) bool {
 	switch r {
 	case '+':
@@ -191,9 +199,7 @@ func subsequent(r rune) bool {
 }
 
 func (l *Lexer) readIdentifier() (Lexeme, error) {
-	var (
-		runes []rune
-	)
+	var runes []rune
 	if r, err := l.next(); initial(r) && err == nil {
 		runes = append(runes, r)
 	} else {
@@ -251,9 +257,7 @@ func (l *Lexer) readBoolean() (Lexeme, error) {
 }
 
 func (l *Lexer) readNumber() (Lexeme, error) {
-	var (
-		runes []rune
-	)
+	var runes []rune
 	if r, err := l.next(); digit(r) && err == nil {
 		runes = append(runes, r)
 	} else {
@@ -273,6 +277,67 @@ func (l *Lexer) readNumber() (Lexeme, error) {
 			return nil, err
 		}
 	}
+}
+
+func (l *Lexer) readCharacter() (Lexeme, error) {
+	if r, err := l.next(); r != '#' {
+		l.back()
+		return nil, err
+	}
+	if r, err := l.next(); r != '\\' {
+		l.back()
+		l.back()
+		return nil, err
+	}
+	r, err := l.next()
+	if err != nil {
+		l.back()
+		l.back()
+		l.back()
+		return nil, err
+	}
+	first := r
+	if r, err := l.next(); (delimiter(r) && err == nil) || err == io.EOF {
+		log.Printf("delimiter %#v", string(r))
+		l.back()
+		return Character(r), err
+	} else if first == 'x' {
+		var hexDigits []byte
+		for {
+			if r, err := l.next(); hexDigit(r) && err == nil {
+				hexDigits = append(hexDigits, r)
+			} else if (delimiter(r) && err == nil) || err == io.EOF {
+				if len(hexDigits) % 2 == 1 {
+					hexDigits = append([]byte{'0'}, hexDigits...)
+				}
+				var bytes []byte
+				hex.Decode(bytes, hexDigits)
+				var i
+				for i, byte := bytes {
+					if byte != 0 {
+						break
+					}
+				}
+				bytes = bytes[i:]
+				if len(bytes) > 4 {
+					return nil, fmt.Errorf("character out of range: %v bytes for maximum 4", len(bytes))
+				}
+				r = 0
+				for _, byte := range bytes {
+					r <<= 1
+					r += rune(byte)
+				}
+				return Character(r), err
+			} else if err == nil {
+				return nil, fmt.Errorf("unexpected rune: %#v", string(r))
+			} else {
+				return nil, err
+			}
+		}
+	} else {
+		return nil, fmt.Errorf("TODO")
+	}
+
 }
 
 func (l *Lexer) readLexeme() (Lexeme, error) {
