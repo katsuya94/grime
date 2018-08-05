62f883326e3ad338331ffc2514b8189f2ca4f884
atateno <adrien.tateno@getbraintree.com>
2018-08-05 09:32:57 -0400

    Move lex to its own directory

--- lex/lex.go
+++ lex/lex.go
@@ -14,6 +14,7 @@ type Identifier string
 type Boolean bool
 type Number string
 type Character rune
+type String string
 type LeftParenthesis struct{}
 type RightParenthesis struct{}
 type LeftBracket struct{}
