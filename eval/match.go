2d2158412e3df3875e483357d3662ea5569bda8b
atateno <adrien.tateno@getbraintree.com>
2018-08-17 17:01:43 -0500

    WIP

--- eval/match.go
+++ eval/match.go
@@ -1,6 +1,9 @@
 package eval
 
-import "github.com/katsuya94/grime/core"
+import (
+	"github.com/katsuya94/grime/core"
+	"fmt"
+)
 
 type Pattern struct {
 	literals map[core.Symbol]Binding
@@ -8,5 +11,14 @@ type Pattern struct {
 }
 
 func Match(in core.Datum, p Pattern) (map[core.Symbol]interface{}, bool, error) {
-	return nil, false, nil
+	switch v := p.pattern.(type) {
+	case core.Symbol:
+		if binding, ok := p.literals[v]; ok {
+			return nil, false, Errorf("match: matching literals not implemented")
+		} else {
+
+		}
+	default:
+		return nil, false, Errorf("match: unhandled datum %#v", v)
+	}
 }
