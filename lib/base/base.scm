7a6ce751d42438ab636014ad5e9474b8ef4f20c4
atateno <adrien.tateno@getbraintree.com>
2018-11-07 16:55:53 -0600

    Add some scheme code to base

--- lib/base/base.scm
+++ lib/base/base.scm
@@ -22,7 +22,12 @@
     eqv?
     syntax->datum
     eq?
-    equal?)
+    equal?
+    or
+    list?
+    fold-left
+    for-all
+    syntax-rules)
   (import (for (core) run expand))
 
   (define (eq? l r)
@@ -30,3 +35,44 @@
 
   (define (equal? l r)
     (eqv? l r)))
+
+  (define-syntax when
+    (lambda (x)
+      (syntax-case x ()
+        [(_ test body ...) #'(if test (begin body ...) (void))])))
+
+  (define-syntax unless
+    (lambda (x)
+      (syntax-case x ()
+        [(_ test body ...) #'(when (not test) body ...)])))
+
+  (define-syntax or
+    (lambda (x)
+      (syntax-case x ()
+        [(_ ) #’#f]
+        [(_ e) #’e]
+        [(_ e1 e2 e3 ...)
+         #’(let ([t e1])
+         (if t t (or e2 e3 ...)))])))
+  
+  (define (list? x)
+    (or (null? x) (pair? x)))
+  
+  (define (fold-left combine nil lst)
+    (unless (proc? combine) (error "fold-left: expected proc"))
+    (unless (list? lst) (error "fold-left: expected list"))
+    (if (null? lst)
+      nil
+      (fold-left combine (combine nil (car lst)) (cdr lst))))
+
+  (define (for-all proc lst)
+    (fold-left (lambda (b x) (and b (proc x))) #t lst))
+
+  (define-syntax syntax-rules
+    (lambda (x)
+      (syntax-case x ()
+        [(_ (lit ...) [(k . p) t] ...)
+         (for-all identifier? #'(lit ... k ...))
+         #'(lambda (x)
+             (syntax-case x (lit ...)
+               [(_ . p) #'t] ...))])))
