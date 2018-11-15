67ba1b624ce16a08b540c66da5206c1df1a32d2f
atateno <adrien.tateno@getbraintree.com>
2018-11-15 13:14:34 -0600

    Continue writing generator

--- gen/gen.go
+++ gen/gen.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"go/types"
 	"os"
 
 	"golang.org/x/tools/go/packages"
 )
 
 func main() {
-	packs, err := packages.Load(&packages.Config{Mode: packages.LoadTypes}, "fmt")
+	err := generate("fmt")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	fmt.Printf("%#v\n", packs[0].Types.Scope().Lookup("Print"))
+}
+
+func generate(patterns ...string) error {
+	pkgs, err := packages.Load(&packages.Config{Mode: packages.LoadTypes}, patterns...)
+	if err != nil {
+		return err
+	}
+	if len(pkgs) != 1 {
+		return fmt.Errorf("found %v packages", len(pkgs))
+	}
+	pkg := pkgs[0]
+	fileSet := token.NewFileSet()
+	fileName := fmt.Sprintf("%v.go", pkg.Name)
+	src := fmt.Sprintf("package %v", pkg.Name)
+	file, err := parser.ParseFile(fileSet, fileName, src, 0)
+	if err != nil {
+		return err
+	}
+	_, err = ast.NewPackage(fileSet, map[string]*ast.File{fileName: file}, nil, nil)
+	if err != nil {
+		return err
+	}
+	for _, name := range pkg.Types.Scope().Names() {
+		object := pkg.Types.Scope().Lookup(name)
+		if !object.Exported() {
+			continue
+		}
+		fmt.Println(types.ObjectString(object, nil))
+	}
+	return format.Node(os.Stdout, fileSet, file)
 }
