5ee8b2959531487cadc9039a3da5d2e529c27676
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2018-11-18 00:56:06 -0600

    Finish implementing gen v1

--- gen/gen.go
+++ gen/gen.go
@@ -9,8 +9,9 @@ import (
 	"go/parser"
 	"go/token"
 	"go/types"
-	"math"
 	"os"
+	"os/exec"
+	"path"
 	"regexp"
 	"strings"
 	"text/template"
@@ -20,32 +21,98 @@ import (
 )
 
 func main() {
-	err := generate("fmt")
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: gen [package]")
+		os.Exit(1)
+	}
+	err := generate(os.Args[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-type fileData struct {
-	Name            string
-	PkgPath         string
-	PkgPathSegments []string
-	Funcs           []funcData
+type Package struct {
+	*packages.Package
+	funcs []*Func
 }
 
-type funcData struct {
-	Name         string
-	InternalName string
-	Params       []paramData
+func NewPackage(pkg *packages.Package) *Package {
+	return &Package{pkg, nil}
 }
 
-type paramData struct {
-	Name string
-	Type string
+func (pkg Package) PkgPathSegments() []string {
+	return strings.Split(pkg.PkgPath, "/")
 }
 
-var fileTemplate = template.Must(template.New("file").Parse(`
+func (pkg Package) Funcs() []*Func {
+	if pkg.funcs == nil {
+		pkg.funcs = []*Func{}
+		scope := pkg.Types.Scope()
+		for _, name := range scope.Names() {
+			f, ok := scope.Lookup(name).(*types.Func)
+			if !ok || !f.Exported() {
+				continue
+			}
+			if f.Type().(*types.Signature).Recv() != nil {
+				fmt.Printf("skipping %v because of receiver\n", f)
+				continue
+			}
+			pkg.funcs = append(pkg.funcs, NewFunc(f))
+		}
+	}
+	return pkg.funcs
+}
+
+type Func struct {
+	*types.Func
+	params []*Var
+}
+
+func NewFunc(f *types.Func) *Func {
+	return &Func{f, nil}
+}
+
+func (f Func) InternalName() string {
+	internalName := f.Name()
+	internalName = string(regexp.MustCompile("[^a-zA-Z0-9]").ReplaceAll([]byte(internalName), []byte{}))
+	internalName = string(append([]rune{unicode.ToLower([]rune(internalName)[0])}, []rune(internalName)[1:]...))
+	return internalName
+}
+
+func (f Func) Params() []*Var {
+	if f.params == nil {
+		f.params = []*Var{}
+		signature := f.Type().(*types.Signature)
+		params := signature.Params()
+		for i := 0; i < params.Len(); i++ {
+			f.params = append(f.params, &Var{params.At(i), false})
+		}
+		if signature.Variadic() {
+			f.params[len(f.params)-1].Variadic = true
+		}
+	}
+	return f.params
+}
+
+func (f Func) ParamTypes() string {
+	var paramTypes []string
+	for _, param := range f.Params() {
+		if param.Variadic {
+			paramTypes = append(paramTypes, fmt.Sprintf("...%v", param.Type().(*types.Slice).Elem()))
+		} else {
+			paramTypes = append(paramTypes, param.Type().String())
+		}
+	}
+	return strings.Join(paramTypes, ", ")
+}
+
+type Var struct {
+	*types.Var
+	Variadic bool
+}
+
+var fileTemplate = template.Must(template.New("package").Parse(`
 package {{.Name}}
 
 import (
@@ -58,7 +125,7 @@ import (
 )
 
 var Library *runtime.Library = runtime.MustNewEmptyLibrary([]common.Symbol{
-	{{range .PkgPathSegments}}
+	{{range .PkgPathSegments -}}
 	common.Symbol("{{.}}"),
 	{{end}}
 }, []int{})
@@ -67,30 +134,45 @@ var Bindings common.BindingSet
 
 func init() {
 	env := common.EmptyEnvironment
-	{{range .Funcs}}
+	{{range .Funcs -}}
 	env = env.MustDefine(common.Symbol("{{.Name}}"), []int{0}, &common.Keyword{common.Function({{.InternalName}})})
-	{{end}}
+	{{end -}}
 	Bindings = env.Bindings()
 }
 
 {{range .Funcs}}
 func {{.InternalName}}(c common.Continuation, args ...common.Datum) (common.EvaluationResult, error) {
+	{{if len .Params -}}
 	valid := true
-	{{range $i := .Params}}
-	{{.Name}}, ok := args[{{$i}}].({{.Type}})
-	valid &&= ok
-	{{end}}
-	if !valid {
-		types := make([]string, len(args))
-		for _, arg := range args {
-			types = append(types, reflect.TypeOf(arg).String())
-		}
-		return common.ErrorC(fmt.Errorf("{{.Name}}: expected ({{range $index := .Params}}{{if ne $index 0}}, {{end}}{{.Type}}{{end}}) got %v", strings.Join(types, ", ")))
+	{{range $i, $param := .Params -}}
+	{{if $param.Variadic -}}
+	var {{$param.Name}} {{$param.Type}}
+	for _, arg := range args[{{$i}}:] {
+		elem, ok := arg.({{$param.Type.Elem}})
+		valid = valid && ok
+		{{$param.Name}} = append({{$param.Name}}, elem)
 	}
-	{{.Name}}(
-		{{range $i := .Params}}
+	{{else -}}
+	{{$param.Name}}, ok := args[{{$i}}].({{$param.Type}})
+	valid = valid && ok
+	{{end -}}
+	{{end -}}
+	if !valid {
+		var argTypes []string
+		for _, arg := range args {
+			argTypes = append(argTypes, reflect.TypeOf(arg).String())
+		}
+		return common.ErrorC(fmt.Errorf("{{.Name}}: expected ({{.ParamTypes}}) got (%v)", strings.Join(argTypes, ", ")))
+	}
+	{{end -}}
+	{{.FullName}}(
+		{{range .Params -}}
+		{{if .Variadic -}}
+		{{.Name}}...,
+		{{else -}}
 		{{.Name}},
-		{{end}}
+		{{end -}}
+		{{end -}}
 	)
 	return common.CallC(c, common.Void)
 }
@@ -102,55 +184,10 @@ func generate(pattern string) error {
 	if err != nil {
 		return err
 	}
-	data := fileData{
-		Name:            pkg.Name,
-		PkgPath:         pkg.PkgPath,
-		PkgPathSegments: strings.Split(pkg.PkgPath, "/"),
-	}
-	scope := pkg.Types.Scope()
-	for _, name := range scope.Names() {
-		object := scope.Lookup(name)
-		if !object.Exported() {
-			continue
-		}
-		switch object := object.(type) {
-		case *types.TypeName:
-		case *types.Func:
-			signature := object.Type().(*types.Signature)
-			if signature.Recv() != nil {
-				fmt.Printf("skipping %v because of receiver\n", object)
-				continue
-			}
-			name := object.FullName()
-			internalName := object.Name()
-			internalName = string(regexp.MustCompile("[^a-zA-Z0-9]").ReplaceAll([]byte(internalName), []byte{}))
-			internalName = string(append([]rune{unicode.ToLower([]rune(internalName)[0])}, []rune(internalName)[1:]...))
-			params := make([]paramData, signature.Params().Len())
-			for i := 0; i < signature.Params().Len(); i++ {
-				params = append(params, paramData{
-					Name: signature.Params().At(i).Name(),
-					Type: signature.Params().At(i).Type().String(),
-				})
-			}
-			data.Funcs = append(data.Funcs, funcData{
-				Name:         name,
-				InternalName: internalName,
-				Params:       params,
-			})
-		default:
-			fmt.Println(object)
-		}
-	}
 	buf := &bytes.Buffer{}
-	err = fileTemplate.Execute(buf, data)
+	err = fileTemplate.Execute(buf, NewPackage(pkg))
 	if err != nil {
 		return err
-	}
-	lines := strings.Split(buf.String(), "\n")
-	n := len(lines)
-	s := int(math.Log10(float64(n))) + 1
-	for i, line := range lines {
-		fmt.Printf(fmt.Sprintf("%%%dd: %%v\n", s), i, line)
 	}
 	fset := token.NewFileSet()
 	filename := fmt.Sprintf("%v.go", pkg.Name)
@@ -163,7 +200,35 @@ func generate(pattern string) error {
 	if err != nil {
 		return err
 	}
-	return format.Node(os.Stdout, fset, file)
+	filepath, err := goPath()
+	if err != nil {
+		return err
+	}
+	filepath = path.Join(filepath, "src/github.com/katsuya94/grime/lib", pkg.PkgPath, fmt.Sprintf("%v.go", pkg.Name))
+	err = os.MkdirAll(path.Dir(filepath), 0775)
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		return err
+	}
+	err = format.Node(f, fset, file)
+	if err != nil {
+		return err
+	}
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("wrote %v\n", filepath)
+	return nil
+}
+
+func goPath() (string, error) {
+	cmd := exec.Command("go", "env", "GOPATH")
+	b, err := cmd.Output()
+	return strings.Trim(string(b), "\n"), err
 }
 
 func loadPackage(pattern string) (*packages.Package, error) {
