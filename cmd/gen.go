888cc547a7e398750412b958a91ac5d6cac55ea2
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2019-01-13 20:20:14 -0600

    Refactor gen

--- cmd/gen.go
+++ cmd/gen.go
@@ -12,8 +12,13 @@ func init() {
 var genCmd = &cobra.Command{
 	Use:   "gen",
 	Short: "Generate Grime bindings for a given Go package",
-	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return gen.Generate(args[0])
+		for _, arg := range args {
+			err := gen.Generate(arg)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	},
 }
