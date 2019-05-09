2db4243ecf8aa962dbf645d281b005c34bfcc1e9
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2019-05-08 23:32:15 -0500

    Initial implementation of stack and frame

--- common/stack.go
+++ common/stack.go
@@ -1,7 +1,22 @@
 package common
 
-type Stack interface{}
+type Stack []Frame
+
+func (s Stack) Get(ref StackFrameReference) interface{} {
+	return s[len(s)-1-ref.Stack][ref.Frame]
+}
+
+func (s Stack) Set(ref StackFrameReference, i interface{}) {
+	s[len(s)-1-ref.Stack][ref.Frame] = i
+}
 
 func NewStack() Stack {
 	return nil
+}
+
+type Frame []interface{}
+
+type StackFrameReference struct {
+	Stack int
+	Frame int
 }
