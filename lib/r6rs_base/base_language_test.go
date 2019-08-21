c9ae302ceb1c3ae5845cd4faba8108b7806b92c5
atateno <adrien.tateno@getbraintree.com>
2019-08-21 13:00:00 -0500

    WIP

--- lib/r6rs_base/base_language_test.go
+++ lib/r6rs_base/base_language_test.go
@@ -9,11 +9,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestBaseLanguageLiteral(t *testing.T) {
+	syntax := Introduce(test.Syntax("#t"))
+	expander := BaseExpander{}
+	actual, err := expander.Expand(syntax, BaseEnvironment)
+	require.NoError(t, err)
+	expected := r6rs.Introduce(test.Syntax("(#%literal #t)"))
+	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+}
+
+func TestBaseLanguageIdUnbound(t *testing.T) {
+	syntax := Introduce(test.Syntax("id"))
+	expander := BaseExpander{}
+	actual, err := expander.Expand(syntax, BaseEnvironment)
+	require.NoError(t, err)
+	expected := r6rs.Introduce(test.Syntax("(#%top id)"))
+	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+}
+
 func TestBaseLanguageLambda(t *testing.T) {
 	syntax := Introduce(test.Syntax("(lambda (id) #t)"))
 	expander := BaseExpander{}
 	actual, err := expander.Expand(syntax, BaseEnvironment)
 	require.NoError(t, err)
 	expected := r6rs.Introduce(test.Syntax("(#%lambda (id) (#%sequence (#%literal #t)))"))
+	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+}
+
+func TestBaseLanguageBegin(t *testing.T) {
+	syntax := Introduce(test.Syntax("(begin #t #f)"))
+	expander := BaseExpander{}
+	actual, err := expander.Expand(syntax, BaseEnvironment)
+	require.NoError(t, err)
+	expected := r6rs.Introduce(test.Syntax("(#%sequence (#%literal #t)(#%literal #f))"))
 	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
 }
