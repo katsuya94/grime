9771f2eab60e455796364786357958ca4519a170
atateno <adrien.tateno@getbraintree.com>
2019-08-22 13:34:00 -0500

    WIP

--- lib/r6rs_base/base_language_test.go
+++ lib/r6rs_base/base_language_test.go
@@ -11,23 +11,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func testBaseLanguage(t *testing.T, syntax common.Syntax, env common.Environment, expectedSrc string) {
+	expander := BaseExpander{}
+	actual, err := expander.Expand(syntax, env)
+	require.NoError(t, err)
+	expected := r6rs.Introduce(test.Syntax(expectedSrc))
+	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+}
+
+func testBaseLanguageError(t *testing.T, syntax common.Syntax, env common.Environment, errMsg string) {
+	expander := BaseExpander{}
+	_, err := expander.Expand(syntax, env)
+	assert.EqualError(t, err, errMsg)
+}
+
 func TestBaseLanguageLiteral(t *testing.T) {
 	syntax := Introduce(test.Syntax("#t"))
-	expander := BaseExpander{}
-	actual, err := expander.Expand(syntax, BaseEnvironment)
-	require.NoError(t, err)
-	expected := r6rs.Introduce(test.Syntax("(#%literal #t)"))
-	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+	env := BaseEnvironment
+	expectedSrc := "(#%literal #t)"
+	testBaseLanguage(t, syntax, env, expectedSrc)
 }
 
 func TestBaseLanguageIdBoundOutOfContext(t *testing.T) {
 	id := Introduce(test.Syntax("id")).IdentifierOrDie()
 	scope := common.NewScope()
 	id, _ = common.Bind(id, scope, 0)
+	env := BaseEnvironment
 	syntax := common.NewSyntax(id.WrappedSyntax)
-	expander := BaseExpander{}
-	_, err := expander.Expand(syntax, BaseEnvironment)
-	assert.EqualError(t, err, "out of context: id at (unknown)")
+	errMsg := "out of context: id at (unknown)"
+	testBaseLanguageError(t, syntax, env, errMsg)
 }
 
 func TestBaseLanguageIdBoundNonVariable(t *testing.T) {
@@ -37,9 +49,8 @@ func TestBaseLanguageIdBoundNonVariable(t *testing.T) {
 	env := BaseEnvironment
 	(&env).Extend(binding, common.NewSyntacticAbstraction(nil))
 	syntax := common.NewSyntax(id.WrappedSyntax)
-	expander := BaseExpander{}
-	_, err := expander.Expand(syntax, env)
-	assert.EqualError(t, err, "non-variable identifier in value context: id is SyntacticAbstraction{<nil>} at (unknown)")
+	errMsg := "non-variable identifier in value context: id is SyntacticAbstraction{<nil>} at (unknown)"
+	testBaseLanguageError(t, syntax, env, errMsg)
 }
 
 func TestBaseLanguageIdBoundInContext(t *testing.T) {
@@ -49,36 +60,27 @@ func TestBaseLanguageIdBoundInContext(t *testing.T) {
 	env := BaseEnvironment
 	(&env).Extend(binding, common.NewVariable())
 	syntax := common.NewSyntax(id.WrappedSyntax)
-	expander := BaseExpander{}
-	actual, err := expander.Expand(syntax, env)
-	require.NoError(t, err)
-	expected := r6rs.Introduce(test.Syntax("(#%load id)"))
-	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+	expectedSrc := "(#%load id)"
+	testBaseLanguage(t, syntax, env, expectedSrc)
 }
 
 func TestBaseLanguageIdUnbound(t *testing.T) {
+	env := BaseEnvironment
 	syntax := Introduce(test.Syntax("id"))
-	expander := BaseExpander{}
-	actual, err := expander.Expand(syntax, BaseEnvironment)
-	require.NoError(t, err)
-	expected := r6rs.Introduce(test.Syntax("(#%top id)"))
-	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+	expectedSrc := "(#%top id)"
+	testBaseLanguage(t, syntax, env, expectedSrc)
 }
 
 func TestBaseLanguageLambda(t *testing.T) {
+	env := BaseEnvironment
 	syntax := Introduce(test.Syntax("(lambda (id) #t)"))
-	expander := BaseExpander{}
-	actual, err := expander.Expand(syntax, BaseEnvironment)
-	require.NoError(t, err)
-	expected := r6rs.Introduce(test.Syntax("(#%lambda (id) (#%sequence (#%literal #t)))"))
-	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+	expectedSrc := "(#%lambda (id) (#%sequence (#%literal #t)))"
+	testBaseLanguage(t, syntax, env, expectedSrc)
 }
 
 func TestBaseLanguageBeginSimple(t *testing.T) {
+	env := BaseEnvironment
 	syntax := Introduce(test.Syntax("(begin #t #f)"))
-	expander := BaseExpander{}
-	actual, err := expander.Expand(syntax, BaseEnvironment)
-	require.NoError(t, err)
-	expected := r6rs.Introduce(test.Syntax("(#%sequence (#%literal #t) (#%literal #f))"))
-	test.AssertCoreSyntaxEqual(t, expected, actual.Unexpand())
+	expectedSrc := "(#%sequence (#%literal #t) (#%literal #f))"
+	testBaseLanguage(t, syntax, env, expectedSrc)
 }
