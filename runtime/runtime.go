712436ebaf65d9d99102c5b0afe91491ea3f2c00
Adrien Tateno <adrien.k.tateno@gmail.com>
2019-08-31 22:11:22 -0500

    WIP

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Runtime struct {
-	libraries []*Library
+	libraries map[string]*Library
 }
 
 func NewRuntime() *Runtime {
@@ -17,24 +17,60 @@ func NewRuntime() *Runtime {
 
 var patternTopLevelProgramImportForm = common.MustCompileSimplePattern(read.MustReadDatum("(import import-spec ...)"), common.Symbol("import"))
 
-func (r *Runtime) Runtime(topLevelProgram []common.Syntax, nullSourceLocationTree common.SourceLocationTree) error {
+func (r *Runtime) Run(topLevelProgram []common.Syntax, nullSourceLocationTree common.SourceLocationTree) error {
 	result, ok := patternTopLevelProgramImportForm.Match(topLevelProgram[0])
 	if !ok {
 		return fmt.Errorf("runtime: malformed top-level program import form")
 	}
-	var library Library
+	var importSpecs []importSpec
 	for _, d := range result[common.Symbol("import-spec")].([]interface{}) {
 		importSpec, err := newImportSpec(d.(common.Syntax))
 		if err != nil {
 			return nil
 		}
-		library.importSpecs = append(library.importSpecs, importSpec)
+		importSpecs = append(importSpecs, importSpec)
 	}
-	library.body = topLevelProgram[1:]
-	library.nullSourceLocationTree = nullSourceLocationTree
-	err := r.Provide(&library)
-	if err != nil {
-		return err
+	body := topLevelProgram[1:]
+	return &run{r, nil}.runWithImports(body, importSpecs)
+}
+
+func (r *Runtime) findLibrary(libraryName []common.Symbol) *Library {
+	return r.libraries[nameString(libraryName)]
+}
+
+type run struct {
+	runtime       *Runtime
+	libraryScopes map[string]*common.Scope
+}
+
+func (r *run) runWithImports(body []common.Syntax, importSpecs []importSpec) error {
+	for _, importSpec := range importSpecs {
+		scope := r.libraryScope(importSpec.libraryName)
 	}
-	return r.instantiate(r.provisions["()"])
+}
+
+func (r *run) libraryScope(name []common.Symbol) *Scope {
+	scope := r.libraryScopes[nameString(importSpec.libraryName())]
+	if scope == nil {
+	}
+}
+
+func nameString(name []common.Symbol) string {
+	return common.Write(list(nameData(name)...))
+}
+
+func nameData(name []common.Symbol) []common.Datum {
+	var data []common.Datum
+	for _, symbol := range name {
+		data = append(data, symbol)
+	}
+	return data
+}
+
+func list(data ...common.Datum) common.Datum {
+	if len(data) == 0 {
+		return common.Null
+	} else {
+		return common.Pair{data[0], list(data[1:]...)}
+	}
 }
